18c1624a435e5abedbe2c7816452dbcd107a8f7a
joefrost01 <joe_frost@mailcan.com>
2025-02-04 19:30:32 +0000

    Built site for gh-pages

--- src/generator/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod market;
-
-pub trait Generator {
-    /// Generate a JSON data message
-    fn generate(&mut self) -> String;
-    /// Dump the internal state
-    fn dump(&self) -> String;
-}
-
